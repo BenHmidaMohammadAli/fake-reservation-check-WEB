cb53168c1d985da886ffd78282c19434e0797a67
unknown <muhammadalibenhmida@gmail.com>
2023-05-07 16:47:02 +0100

    Update

--- check_fake_reservation/dashboard/models.py
+++ check_fake_reservation/dashboard/models.py
@@ -10,7 +10,8 @@ class Flight(models.Model):
     flight_end = models.DateTimeField(null=True) 
     From = models.CharField(max_length=200, null=True)
     To = models.CharField(max_length=200, null=True)
-    available = models.BooleanField(null = True, blank=True)  
+    available = models.BooleanField(null = True, blank=True) 
+    route = models.CharField(max_length=200, null=True)
     flight_pic = models.ImageField(null = True, blank=True)  
     date_created = models.DateTimeField(auto_now_add= True, null=True)  
     def __str__(self) :
@@ -18,14 +19,18 @@ class Flight(models.Model):
     
 class ReservationFlight(models.Model):
     passenger = models.OneToOneField(Passenger, null=True, on_delete=models.CASCADE)
+    sales_channel = models.CharField(max_length=200, null =True , blank= True)
+    nbr_seat = models.IntegerField(null= True , blank= True )
     flight = models.OneToOneField(Flight, null=True, on_delete=models.CASCADE)
     nbr_days_stay = models.IntegerField(null = True, blank=True)
     extra_baggage = models.BooleanField(null = True, blank=True) 
     meal = models.BooleanField(null = True, blank=True) 
     preffered_seat = models.BooleanField(null = True, blank=True) 
     nbr_seat = models.IntegerField(null = True, blank=True)
+    type = models.CharField(max_length=200, null=True)
+    orign = models.CharField(max_length=200,null = True , blank= True )
     state = models.BooleanField(null = True, blank=True)
-
+    
 class ReservationFlightPredictions(models.Model):
     reservationFlight = models.OneToOneField(ReservationFlight, null=True, on_delete=models.CASCADE)
     complete = models.BooleanField(null = True, blank=True)
